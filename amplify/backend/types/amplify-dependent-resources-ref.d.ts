ade86111b6e1fa9ca37a5cccbe9777eb01e4549a
Udeh <udeho4567@gmail.com>
2023-02-18 17:40:20 -0500

    Added api and database

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyappe8130497": {
       "AppClientID": "string",
